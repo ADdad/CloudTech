761899806573a4aa8b25c4b84d300c1d38f1d9df
ADdad <rokden80@gmail.com>
2017-02-19 20:16:05 +0200

    Init

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+
+
+public class Test
+{
+
+    public static void main(String[] args) {
+        System.out.println("Free memory: "+ Runtime.getRuntime().freeMemory());
+        long l = System.currentTimeMillis();
+        Collection c = new Collection("C:\\dict");
+        SPIMI s = new SPIMI(c.getCollection());
+        long l2 = System.currentTimeMillis();
+        System.out.println("Blocked: "+(l-l2));
+      Merge m = new Merge("C:\\blocks");
+        long l3 = System.currentTimeMillis();
+        System.out.println("Merged: "+(l2-l3));
+        System.out.println("Total time: "+(l-l3));
+
+    }
+}
