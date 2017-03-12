304f96f2234e3b21f5fbbfb6c6444ec1aab8b1e0
ADdad <rokden80@gmail.com>
2017-03-12 18:50:46 +0200

    created compressing methods and key variable

--- src/DictCompress.java
+++ src/DictCompress.java
@@ -7,10 +7,13 @@ public class DictCompress {
     int block_size = 4;
     SortedMap<String, ArrayList<Integer>> sortedMap;
     ArrayList<String> compressed;
+    char key = 126;
 
     DictCompress(HashMap<String, ArrayList<Integer>> dict){
         sortedMap = new TreeMap();
         sortedMap.putAll(dict);
+    }
+    DictCompress(){
     }
 
     void compressDict(){
@@ -30,6 +33,28 @@ public class DictCompress {
 
     void addBlock(String s){
         String[] bl = s.split(" ");
+        int pref = findPref(bl);
+        String res = bl[0].substring(0, pref)+"*"+bl[0].substring(pref);
+        for (int i = 1; i < bl.length; i++) {
+            res+=(bl[i].length()-pref);
+            res+=key+bl[i].substring(pref);
+        }
+        compressed.add(res);
+    }
+
+    int findPref(String[] s) {
+        int min_size = s[0].length();
+        for (int j = 1; j < s.length; j++) {
+            if(s[j].length()<min_size)min_size=s[j].length();
+        }
+        for (int i = 0; i < min_size; i++) {
+            boolean same = false;
+            for (int j = 1; j < s.length; j++) {
+                if(s[0].charAt(i)!=s[j].charAt(i))same=!same;
+            }
+            if (same)return (i);
+        }
+        return 0;
     }
 
 }
